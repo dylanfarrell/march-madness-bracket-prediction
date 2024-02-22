8b71fdcf3cf48e311f1ebf77a576978b7faea6f0
Daniel Alpert <dalpert89@gmail.com>
2024-02-21 23:49:24 -0800

    set current year to 2023

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
-CURRENT_YR = datetime.now().year
+CURRENT_YR = 2023
 KAGGLE_DIR = f"data/{CURRENT_YR}/kaggle_data"
 GENERATED_DIR = f"data/{CURRENT_YR}/generated_data"
