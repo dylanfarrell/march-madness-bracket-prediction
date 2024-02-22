e07c3906d07049b0c17f1d2fb1c0c71d742c9f65
Daniel Alpert <dalpert89@gmail.com>
2024-02-22 00:24:58 -0800

    add type annotations

--- helper_functions.py
+++ helper_functions.py
@@ -3,21 +3,21 @@ import pandas as pd
 from datetime import datetime
 from constants import KAGGLE_DIR
 
+
 # retrieve team name by id
-def team_lookup(id_num):
+def team_lookup(id: int) -> str:
     """
     Retrieves a team name given its id
 
     :param id_num: kaggle team id (int)
     :return: team name (str)
     """
-    print(f"{KAGGLE_DIR}/MTeams.csv")
     teams = pd.read_csv(f"{KAGGLE_DIR}/MTeams.csv")
-    return teams[teams["TeamID"] == id_num]["TeamName"].values[0]
+    return teams[teams["TeamID"] == id]["TeamName"].values[0]
 
 
 # add team spellings to the team spellings csv
-def update_team_spellings_file(unmatched_spellings_lst):
+def update_team_spellings_file(unmatched_spellings_lst: list[tuple[str, int]]) -> None:
     """
     Takes in a list of (team spelling, team id) tuples and adds them to
     the list of valid team spellings
@@ -40,7 +40,9 @@ def update_team_spellings_file(unmatched_spellings_lst):
 
 
 # join outside data source to team spellings csv to get kaggle team id
-def scraped_df_join_to_team_spellings(scraped_df, team_col):
+def scraped_df_join_to_team_spellings(
+    scraped_df: pd.DataFrame, team_col: str = "season"
+) -> pd.DataFrame:
     """
     Joins a scraped table without kaggle team ids to the kaggle team spellings file
     to get the associated team id
@@ -62,10 +64,12 @@ def scraped_df_join_to_team_spellings(scraped_df, team_col):
 
 
 # outputs the team spellings from the scraped table that don't appear in the kaggle table
-def check_for_missing_spellings(scraped_df, joined_df, team_col):
+def check_for_missing_spellings(
+    scraped_df: pd.DataFrame, joined_df: pd.DataFrame, team_col: str = "season"
+) -> list:
     """
     Rejoins the joined table to the scraped table. Honestly should handle this all
     in the scraped_df_join function but will handle that next year.
     """
     comp = scraped_df.merge(joined_df, on=team_col, how="left")
-    return np.unique(comp[comp["TeamID"].isna()][team_col])
+    return list(np.unique(comp[comp["TeamID"].isna()][team_col]))
