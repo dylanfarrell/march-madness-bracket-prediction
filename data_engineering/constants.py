7a4cf91faf991aeec84dc06549d467ed3c9a41cd
Daniel Alpert <dalpert89@gmail.com>
2024-02-28 23:07:56 -0800

    change constants to go one level up

--- data_engineering/constants.py
+++ data_engineering/constants.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 CURRENT_YR = 2024
 DATA_START_YR = 2003
-KAGGLE_DIR = f"data/{CURRENT_YR}/kaggle_data"
-KAGGLE_DIR_LAST_YR = f"data/{CURRENT_YR - 1}/kaggle_data"
-GENERATED_DIR = f"data/{CURRENT_YR}/generated_data"
-GENERATED_DIR_LAST_YR = f"data/{CURRENT_YR - 1}/generated_data"
+KAGGLE_DIR = f"../data/{CURRENT_YR}/kaggle_data"
+KAGGLE_DIR_LAST_YR = f"../data/{CURRENT_YR - 1}/kaggle_data"
+GENERATED_DIR = f"../data/{CURRENT_YR}/generated_data"
+GENERATED_DIR_LAST_YR = f"../data/{CURRENT_YR - 1}/generated_data"
