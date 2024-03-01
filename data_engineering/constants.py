2bdbaf741396ded19a43bb17a61c35ef40ae4664
Daniel Alpert <dalpert89@gmail.com>
2024-02-29 23:49:59 -0800

    add sports ref stub constant

--- data_engineering/constants.py
+++ data_engineering/constants.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
+# Years
 CURRENT_YR = 2024
 DATA_START_YR = 2003
+
+# Directories
 KAGGLE_DIR = f"../data/{CURRENT_YR}/kaggle_data"
 KAGGLE_DIR_LAST_YR = f"../data/{CURRENT_YR - 1}/kaggle_data"
 GENERATED_DIR = f"../data/{CURRENT_YR}/generated_data"
 GENERATED_DIR_LAST_YR = f"../data/{CURRENT_YR - 1}/generated_data"
+
+# Sports Reference
+SPORTS_REF_STUB = "https://www.sports-reference.com"
