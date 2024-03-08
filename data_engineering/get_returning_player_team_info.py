84cb7f866d304286f0f6774415243de1d9f50bb8
Daniel Alpert <dalpert89@gmail.com>
2024-03-07 21:55:29 -0800

    refactor with simpler functions

--- data_engineering/get_returning_player_team_info.py
+++ data_engineering/get_returning_player_team_info.py
@@ -1,9 +1,10 @@
 import re
 import pandas as pd
+from tqdm import tqdm
 
 from argparser_config import get_parsed_args
 import helper_functions as hf
-from constants import SPORTS_REF_STUB
+from constants import SPORTS_REF_STUB, CURRENT_YR
 
 
 def get_returning_team_stats(team):
@@ -16,32 +17,21 @@ def get_returning_team_stats(team):
     return (returning_min_pct, returning_score_pct)
 
 
-def get_returning_player_team_info(yr):
-    link = f"{SPORTS_REF_STUB}/cbb/seasons/{yr}-coaches.html"
-    soup = hf.get_soup(link)
+def get_all_returning_info(teams, year=CURRENT_YR, overwrite=False) -> list[str]:
+    # list to log teams that failed to get data
+    failed_teams = []
 
-    coach_table = soup.find("table", {"id": "coaches"})
-    coach_tbody = coach_table.find("tbody")
-    coach_rows = coach_tbody.find_all("tr")
-
-    coach_rows_lst = []
-    for coach_row in coach_rows:
-        tds = coach_row.find_all("td")
-        # some rows with minimal tds are not actual data rows
-        if len(tds) > 10:
-            team_stub = tds[0].find("a")["href"]
-            school = team_stub.split("/")[3]
-            team_link = f"{SPORTS_REF_STUB}{team_stub}"
-            print(school)
-            team_soup = hf.get_soup(team_link, rate_limit=True)
-            text_div = team_soup.find("div", {"id": "tfooter_roster"}).text
-            decimals = re.findall("\d+\.\d", text_div)
-            returning_min_pct = float(decimals[0])
-            returning_score_pct = float(decimals[1])
-            row = [yr, school, returning_min_pct, returning_score_pct]
-            coach_rows_lst.append(row)
-    cols = ["Season", "school", "returning_min_pct", "returning_score_pct"]
-    return pd.DataFrame(data=coach_rows_lst, columns=cols)
+    team_info = []
+    for team in tqdm(teams, desc="Iterating throughs all teams:"):
+        try:
+            info = get_returning_team_stats(team)
+            team_info.append([team, *info])
+        except Exception as e:
+            # Catch any other errors that occur and log the team's name
+            failed_teams.append(team)
+    cols = ["team", "returning_min_pct", "returning_score_pct"]
+    df = pd.DataFrame(team_info, columns=cols)
+    return df, failed_teams
 
 
 def main():
@@ -49,7 +39,7 @@ def main():
     args = get_parsed_args()
 
     # Call the function with the command-line arguments
-    df = get_returning_player_team_info(yr=args.year)
+    df = get_all_returning_info(yr=args.year)
 
     file_path = f"{hf.get_generated_dir(args.year)}/returning_player_team_stats.csv"
     hf.write_to_csv(df, file_path, args.overwrite)
