688d9168193222ba4897604ccaee8c8714a558b3
Daniel Alpert <dalpert89@gmail.com>
2024-03-13 12:49:02 -0700

    add generated datasets to constants

--- data_engineering/constants.py
+++ data_engineering/constants.py
@@ -7,6 +7,16 @@ KAGGLE_DIR = f"../data/{CURRENT_YR}/kaggle_data"
 KAGGLE_DIR_LAST_YR = f"../data/{CURRENT_YR - 1}/kaggle_data"
 GENERATED_DIR = f"../data/{CURRENT_YR}/generated_data"
 GENERATED_DIR_LAST_YR = f"../data/{CURRENT_YR - 1}/generated_data"
+KAGGLE_DIR = f"../data/{CURRENT_YR}/silver_data"
 
 # Sports Reference
 SPORTS_REF_STUB = "https://www.sports-reference.com"
+
+GENERATED_DATASETS = [
+    "coaches_data",
+    "preseason_rankings",
+    "returning_player_team_stats_tourney",
+    "sports_ref_team_stats",
+    "team_weighted_info_tourney",
+    "stationary_probabilities",
+]
