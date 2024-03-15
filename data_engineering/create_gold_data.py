fb1d383303c6680d4308a964c9cbcbc33a760d35
Daniel Alpert <dalpert89@gmail.com>
2024-03-14 19:14:52 -0700

    make gold data script more modular

--- data_engineering/create_gold_data.py
+++ data_engineering/create_gold_data.py
@@ -1,48 +1,50 @@
 import pandas as pd
 from argparser_config import get_parser
 import helper_functions as hf
-from constants import GENERATED_DATASETS
+from constants import GENERATED_DATASETS, DATA_START_YR
+
+
+def iteratively_join_data(
+    df: pd.DataFrame, datasets: list[str], year: int
+) -> pd.DataFrame:
+    for i, dataset in enumerate(datasets):
+        dataset = dataset + "_silver"
+        suffixes = (f"_left_{i}", f"_right_{i}")
+        df = pd.merge(
+            df,
+            hf.load_silver_data(dataset, year),
+            on=["TeamID", "year"],
+            how="left",
+            suffixes=suffixes,
+        )
+        print(f"{dataset} is complete.")
+    return df
 
 
 def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    gold_data = hf.load_and_trim("MNCAATourneySeeds", 2003)[
+    gold_data = hf.load_and_trim("MNCAATourneySeeds", DATA_START_YR)[
         ["TeamID", "Season"]
     ].rename({"Season": "year"}, axis=1)
-    print(len(gold_data))
 
-    # Assuming GENERATED_DATASETS is a list of datasets to be loaded and merged
-    if GENERATED_DATASETS:
-        # Iterate over the remaining datasets and merge them with gold_data
-        for i, dataset in enumerate(GENERATED_DATASETS):
-            dataset = dataset + "_silver"
-            suffixes = (f"_left_{i}", f"_right_{i}")
-            print(suffixes)
-            df = hf.load_silver_data(dataset, args.year)
-            gold_data = pd.merge(
-                gold_data,
-                df,
-                on=["TeamID", "year"],
-                how="left",
-                suffixes=suffixes,
-            )
-            print(f"{dataset} is complete.")
+    gold_data = iteratively_join_data(gold_data, GENERATED_DATASETS, args.year)
 
     print(gold_data.columns)
 
     file_path = f"{hf.get_gold_dir(args.year)}/gold_data_external_sources.csv"
     hf.write_to_csv(gold_data, file_path, args.overwrite)
 
-    kaggle_features = hf.load_generated_data("team_season_features")
+    # join features created from kaggle data to gold data
+    kaggle_features = hf.load_generated_data("team_season_features", args.year)
     gold_data_all = pd.merge(
         gold_data,
         kaggle_features,
         how="left",
         left_on=["TeamID", "year"],
         right_on=["TeamID", "Season"],
-        suffixes=suffixes,
+        suffixes=("_left", "_right"),
     )
     file_path = f"{hf.get_gold_dir(args.year)}/gold_data_all.csv"
     hf.write_to_csv(gold_data_all, file_path, args.overwrite)
