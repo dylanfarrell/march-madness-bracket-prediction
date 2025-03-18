724811a06e4c74d57f81bbb6fbf8992b2bf17cc2
Daniel Alpert <dalpert89@gmail.com>
2025-03-18 19:04:32 -0400

    add mode functionality for gold and silver data creation

--- data_engineering/create_gold_data.py
+++ data_engineering/create_gold_data.py
@@ -1,18 +1,18 @@
 import pandas as pd
 from argparser_config import get_parser
 import helper_functions as hf
-from constants import GENERATED_DATASETS, DATA_START_YR
+from constants import WOMENS_GENERATED_DATASETS, MENS_GENERATED_DATASETS, DATA_START_YR
 
 
 def iteratively_join_data(
-    df: pd.DataFrame, datasets: list[str], year: int
+    df: pd.DataFrame, datasets: list[str], year: int, mode: str
 ) -> pd.DataFrame:
     for i, dataset in enumerate(datasets):
         dataset = dataset + "_silver"
         suffixes = (f"_left_{i}", f"_right_{i}")
         df = pd.merge(
             df,
-            hf.load_silver_data(dataset, year),
+            hf.load_silver_data(dataset, year, mode=mode),
             on=["TeamID", "year"],
             how="left",
             suffixes=suffixes,
@@ -24,20 +24,24 @@ def iteratively_join_data(
 def main():
     parser = get_parser()
     args = parser.parse_args()
+    mode = args.mode
 
-    gold_data = hf.load_and_trim("MNCAATourneySeeds")[["TeamID", "Season"]].rename(
-        {"Season": "year"}, axis=1
-    )
+    gold_data = hf.load_and_trim(f"{mode}NCAATourneySeeds", mode=mode)[
+        ["TeamID", "Season"]
+    ].rename({"Season": "year"}, axis=1)
 
-    gold_data = iteratively_join_data(gold_data, GENERATED_DATASETS, args.year)
+    datasets = WOMENS_GENERATED_DATASETS if mode == "W" else MENS_GENERATED_DATASETS
+    gold_data = iteratively_join_data(gold_data, datasets, args.year, mode)
 
     print(gold_data.columns)
 
-    file_path = f"{hf.get_gold_dir(args.year)}/gold_data_external_sources.csv"
+    file_path = (
+        f"{hf.get_gold_dir(args.year, mode=mode)}/gold_data_external_sources.csv"
+    )
     hf.write_to_csv(gold_data, file_path, args.overwrite)
 
     # join features created from kaggle data to gold data
-    kaggle_features = hf.load_generated_data("team_season_features", args.year)
+    kaggle_features = hf.load_generated_data("team_season_features", args.year, mode)
     gold_data_all = pd.merge(
         gold_data,
         kaggle_features,
@@ -46,7 +50,7 @@ def main():
         right_on=["TeamID", "Season"],
         suffixes=("_left", "_right"),
     )
-    file_path = f"{hf.get_gold_dir(args.year)}/gold_data_all.csv"
+    file_path = f"{hf.get_gold_dir(args.year, mode)}/gold_data_all.csv"
     hf.write_to_csv(gold_data_all, file_path, args.overwrite)
 
 
--- data_engineering/create_silver_data.py
+++ data_engineering/create_silver_data.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 import helper_functions as hf
 from argparser_config import get_parser
-from constants import GENERATED_DATASETS
+from constants import MENS_GENERATED_DATASETS, WOMENS_GENERATED_DATASETS
 
 
-def add_kaggle_id(df: pd.DataFrame, year: int, team_col: str = "team") -> pd.DataFrame:
-    team_spellings = hf.load_team_spellings(year)
+def add_kaggle_id(
+    df: pd.DataFrame, year: int, mode: str, team_col: str = "team"
+) -> pd.DataFrame:
+    team_spellings = hf.load_team_spellings(year, mode)
     merged_df = df.merge(
         team_spellings, how="inner", left_on=team_col, right_on="TeamNameSpelling"
     )
@@ -33,20 +35,25 @@ def main():
     args = parser.parse_args()
 
     datasets_already_silver = ["stationary_probabilities"]
+    datasets = (
+        WOMENS_GENERATED_DATASETS if args.mode == "W" else MENS_GENERATED_DATASETS
+    )
 
-    for dataset in GENERATED_DATASETS:
+    for dataset in datasets:
         try:
-            df = hf.load_generated_data(dataset, args.year)
+            df = hf.load_generated_data(dataset, args.year, args.mode)
             if dataset not in datasets_already_silver:
                 print(f"Adding kaggle ids to: {dataset}")
-                df_silver = add_kaggle_id(df, args.year)
+                df_silver = add_kaggle_id(df, args.year, args.mode)
             else:
                 print(f"Dataset {dataset} already has kaggle ids.")
                 df_silver = df
-            file_path = f"{hf.get_silver_dir(args.year)}/{dataset}_silver.csv"
+            file_path = (
+                f"{hf.get_silver_dir(args.year, args.mode)}/{dataset}_silver.csv"
+            )
             hf.write_to_csv(df_silver, file_path, args.overwrite)
         except FileNotFoundError:
-            print(f"File for {dataset} not found in {args.year}.")
+            print(f"File for {dataset} not found in {args.year}/{args.mode}.")
 
 
 if __name__ == "__main__":
